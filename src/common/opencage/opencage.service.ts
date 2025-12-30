75421f3f413c6d794ef873e7afc5381ec8268aae
FadelNurAkhmad <76637105+FadelNurAkhmad@users.noreply.github.com>
2025-12-30 15:51:17 +0700

    API User Address 1

---
+++ src/common/opencage/opencage.service.ts
@@ -0,0 +1,39 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { BadRequestException, Injectable } from '@nestjs/common';
+import axios from 'axios';
+
+@Injectable()
+export class OpencageService {
+    async geocode(address: string): Promise<{ lat: number; lng: number }> {
+        const apiKey = process.env.OPENCAGE_API_KEY;
+        if (!apiKey) {
+            throw new Error(
+                'OpenCage API key is not set in environment variables',
+            );
+        }
+        try {
+            const response = await axios.get(
+                'https://api.opencagedata.com/geocode/v1/json',
+                {
+                    params: {
+                        q: address,
+                        key: apiKey,
+                        limit: 1,
+                    },
+                },
+            );
+            const result = response.data.results?.[0];
+            if (!result) {
+                throw new Error('No results found for the given address');
+            }
+            return {
+                lat: result.geometry.lat,
+                lng: result.geometry.lng,
+            };
+        } catch (error) {
+            console.error('Error during geocoding:', error);
+            throw new BadRequestException('Failed to geocode the address');
+        }
+    }
+}
